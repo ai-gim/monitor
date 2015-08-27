ed269f29ee68db8ac809d5a2244afb465329adc4
wangjy <wangjy7@yourmaili.com>
2015-08-27 11:45:49 +0800

    1.修改dom4j版本 2.sql脚本分类 3.main函数删除多余逻辑

--- sql/create_objects.sql
+++ sql/create_objects.sql
@@ -1,3 +1,4 @@
+/*cpu index*/
 CREATE TABLE IF NOT EXISTS  `monitor_cpu_used` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
@@ -6,90 +7,6 @@ CREATE TABLE IF NOT EXISTS  `monitor_cpu_used` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS  `monitor_cpu_steal` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_load_one` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_mem_total` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_proc_run` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_load_five` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_disk_free` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_mem_cached` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_pkts_in` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_bytes_in` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_bytes_out` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_swap_total` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_mem_free` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_load_fifteen` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
   `unit` VARCHAR(128) NULL,
@@ -124,35 +41,7 @@ CREATE TABLE IF NOT EXISTS  `monitor_cpu_aidle` (
   `time` DATETIME NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS  `monitor_mem_buffers` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS  `monitor_cpu_system` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_part_max_used` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_disk_total` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS  `monitor_mem_shared` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
   `unit` VARCHAR(128) NULL,
@@ -166,13 +55,6 @@ CREATE TABLE IF NOT EXISTS  `monitor_cpu_wio` (
   `time` DATETIME NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS  `monitor_proc_total` (
-  `ip` VARCHAR(128) NULL,
-  `value` FLOAT NULL,
-  `unit` VARCHAR(128) NULL,
-  `time` DATETIME NULL)
-ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS  `monitor_cpu_speed` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
@@ -180,14 +62,138 @@ CREATE TABLE IF NOT EXISTS  `monitor_cpu_speed` (
   `time` DATETIME NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS  `monitor_pkts_out` (
+/*memory index*/
+CREATE TABLE IF NOT EXISTS  `monitor_mem_total` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
   `unit` VARCHAR(128) NULL,
   `time` DATETIME NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS  `monitor_swap_free` (
+CREATE TABLE IF NOT EXISTS  `monitor_mem_cached` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_mem_swap_total` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_mem_swap_free` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_mem_shared` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_mem_free` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_mem_buffers` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+/*disk index*/
+CREATE TABLE IF NOT EXISTS  `monitor_disk_total` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_disk_free` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_disk_part_max_used` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+/*load index*/
+CREATE TABLE IF NOT EXISTS  `monitor_load_one` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_load_five` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_load_fifteen` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+/*network*/
+CREATE TABLE IF NOT EXISTS  `monitor_network_pkts_in` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_network_pkts_out` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_network_bytes_in` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_network_bytes_out` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+/*process*/
+CREATE TABLE IF NOT EXISTS  `monitor_proc_total` (
+  `ip` VARCHAR(128) NULL,
+  `value` FLOAT NULL,
+  `unit` VARCHAR(128) NULL,
+  `time` DATETIME NULL)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS  `monitor_proc_run` (
   `ip` VARCHAR(128) NULL,
   `value` FLOAT NULL,
   `unit` VARCHAR(128) NULL,
