a52ac606840d01eb7d209cddb220331187fb022a
zhangli <zhangli11@asiainfo.com>
2015-09-24 11:33:24 +0800

    CI脚本修改

---
+++ install/install.sh
@@ -0,0 +1,25 @@
+# cat install.sh 
+#!/bin/bash
+install_dir=/usr/local/lib/gim
+if [[ -e $install_dir ]];then
+    rm -rf $install_dir/monitor
+else
+    mkdir -p $install_dir
+fi
+sed -n '1,/^exit 0$/!p' $0 >$install_dir/monitor.tar.gz
+
+cd $install_dir
+tar -xzvf monitor.tar.gz
+
+if [[ -e /etc/gim ]];then
+    rm -rf /etc/gim/db.conf
+    rm -rf /etc/gim/monitor.conf
+else
+    mkdir /etc/gim
+fi
+
+cd $install_dir/monitor
+cp conf/* /etc/gim
+chmod 755 bin/*
+
+exit 0
