c53363f9b5ed57132ef8bf3c6929ebcd0d8088df
zhangli <zhangli11@asiainfo.com>
2015-09-25 09:41:41 +0800

    安装增加sql脚本执行

--- install/install.sh
+++ install/install.sh
@@ -26,4 +26,12 @@ chmod 755 bin/*
 chmod 755 /etc/rc.d/init.d/gim-monitor
 chkconfig gim-monitor on
 
+path=$(pwd)
+mysql -uroot << EOF
+use gim
+source $path/sql/create_objects.sql;
+EOF
+
+rm -rf $install_dir/monitor.tar.gz
+
 exit 0
