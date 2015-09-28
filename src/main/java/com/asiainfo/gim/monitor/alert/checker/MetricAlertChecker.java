93e6f6054ff0ae4aff76ff8900da5922d802ee10
zhangli <zhangli11@asiainfo.com>
2015-09-28 17:22:48 +0800

    修改不指定告警目标的配置无法正常告警的问题

--- src/main/java/com/asiainfo/gim/monitor/alert/checker/MetricAlertChecker.java
+++ src/main/java/com/asiainfo/gim/monitor/alert/checker/MetricAlertChecker.java
@@ -9,7 +9,9 @@
 package com.asiainfo.gim.monitor.alert.checker;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
@@ -30,16 +32,15 @@ public class MetricAlertChecker extends AlertChecker<Float>
 {
 	private AlertConfig alertConfig;
 
-	private long recordTimeAlert;
-	private long recordTimeRecover;
-	
+	private Map<String, Long> timeRecordMap = new HashMap<String, Long>();
+
 	private ThreadLocal<String> targetIdTh = new ThreadLocal<String>();
 
 	public void setAlertConfig(AlertConfig alertConfig)
 	{
 		this.alertConfig = alertConfig;
 	}
-	
+
 	public void setTargetId(String targetId)
 	{
 		targetIdTh.set(targetId);
@@ -50,12 +51,13 @@ public class MetricAlertChecker extends AlertChecker<Float>
 	{
 		if (isOverThreshold(value) && findCurrentAlert() == null)
 		{
-			if (recordTimeAlert == 0)
+			if (!timeRecordMap.containsKey(targetIdTh.get() + "-alert")
+					|| timeRecordMap.get(targetIdTh.get() + "-alert") == 0)
 			{
-				recordTimeAlert = System.currentTimeMillis();
+				timeRecordMap.put(targetIdTh.get() + "-alert", System.currentTimeMillis());
 			}
-			else if (System.currentTimeMillis() - recordTimeAlert > NumberUtils.toInt(alertConfig.getProperties().get(
-					"keepTime")) * 1000)
+			else if (System.currentTimeMillis() - timeRecordMap.get(targetIdTh.get() + "-alert") > NumberUtils
+					.toInt(alertConfig.getProperties().get("keepTime")) * 1000 * 60)
 			{
 				Alert alert = new Alert();
 				alert.setId(UUID.randomUUID().toString());
@@ -71,7 +73,7 @@ public class MetricAlertChecker extends AlertChecker<Float>
 		}
 		else
 		{
-			recordTimeAlert = 0;
+			timeRecordMap.put(targetIdTh.get() + "-alert", 0L);
 		}
 
 		return null;
@@ -82,15 +84,20 @@ public class MetricAlertChecker extends AlertChecker<Float>
 		Alert alert = findCurrentAlert();
 		if (!isOverThreshold(value) && alert != null)
 		{
-			if (System.currentTimeMillis() - recordTimeRecover > NumberUtils.toInt(alertConfig.getProperties().get(
-					"recoverTime")))
+			if (!timeRecordMap.containsKey(targetIdTh.get() + "-recover")
+					|| timeRecordMap.get(targetIdTh.get() + "-recover") == 0)
+			{
+				timeRecordMap.put(targetIdTh.get() + "-recover", System.currentTimeMillis());
+			}
+			if (System.currentTimeMillis() - timeRecordMap.get(targetIdTh.get() + "-recover") > NumberUtils
+					.toInt(alertConfig.getProperties().get("recoverTime")))
 			{
 				return alert;
 			}
 		}
 		else
 		{
-			recordTimeRecover = 0;
+			timeRecordMap.put(targetIdTh.get() + "-recover", 0L);
 		}
 		return null;
 	}
@@ -182,15 +189,18 @@ public class MetricAlertChecker extends AlertChecker<Float>
 		{
 			if (StringUtils.equals(alertConfig.getProperties().get("thresholdSymbol"), "1"))
 			{
-				return alertConfig.getProperties().get("metric") + "超过" + alertConfig.getProperties().get("thresholdValue") + "%";
+				return alertConfig.getProperties().get("metric") + "超过"
+						+ alertConfig.getProperties().get("thresholdValue") + "%";
 			}
 			else if (StringUtils.equals(alertConfig.getProperties().get("thresholdSymbol"), "3"))
 			{
-				return alertConfig.getProperties().get("metric") + "低于" + alertConfig.getProperties().get("thresholdValue") + "%";
+				return alertConfig.getProperties().get("metric") + "低于"
+						+ alertConfig.getProperties().get("thresholdValue") + "%";
 			}
 			else
 			{
-				return alertConfig.getProperties().get("metric") + "等于" + alertConfig.getProperties().get("thresholdValue") + "%";
+				return alertConfig.getProperties().get("metric") + "等于"
+						+ alertConfig.getProperties().get("thresholdValue") + "%";
 			}
 		}
 	}
