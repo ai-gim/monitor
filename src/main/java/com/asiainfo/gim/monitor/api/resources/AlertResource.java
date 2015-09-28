8eafdc191fe5f7093c61d9b4a13043af3bb4fc1f
鲁洋 <luyang@asiainfo.com>
2015-09-28 16:18:44 +0800

    1.告警确认接口完成。

--- src/main/java/com/asiainfo/gim/monitor/api/resources/AlertResource.java
+++ src/main/java/com/asiainfo/gim/monitor/api/resources/AlertResource.java
@@ -1,6 +1,7 @@
 package com.asiainfo.gim.monitor.api.resources;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -8,12 +9,16 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.asiainfo.gim.common.rest.exception.ResourceNotFoundException;
 import com.asiainfo.gim.common.rest.exception.ValidationException;
 import com.asiainfo.gim.common.spring.SpringContext;
 import com.asiainfo.gim.monitor.domain.Alert;
+import com.asiainfo.gim.monitor.domain.AlertAction;
 import com.asiainfo.gim.monitor.service.AlertService;
 
-@Path("/alerts")
+@Path("/alert")
 @Produces(MediaType.APPLICATION_JSON)
 public class AlertResource
 {
@@ -27,33 +32,35 @@ public class AlertResource
 	@PathParam("id")
 	private String id;
 
+	@GET
+	@Path("{id}")
 	public Alert getAlert()
 	{
 		return alertService.findAlertById(id);
 	}
 
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public void addAlert(Alert alert)
-	{
-		if (alert == null)
-		{
-			throw new ValidationException();
-		}
-		alertService.addAlert(alert);
-	}
-
 	@PUT
-	@Path("{id}")
-	public void updateAlert(Alert alert)
+	@Path("{id}/action")
+	public void confirmAlert(AlertAction alertAction)
 	{
-		if (alert == null)
+		if (alertAction == null)
 		{
 			throw new ValidationException();
 		}
-		alertService.confirmAlert(alert);
+		
+		Alert alert = alertService.findAlertById(id);
+		if (alert == null)
+		{
+			throw new ResourceNotFoundException();
+		}
+		
+		if (StringUtils.equals(alertAction.getAction(), "confirm"))
+		{
+			alertService.confirmAlert(alert);
+		}
+		
 	}
-
+	
 	@DELETE
 	@Path("{id}")
 	public void deleteAlert()
